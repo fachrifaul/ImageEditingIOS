0f0c992add865d827d584ab726abc271e206af4d
Fachri Febrian <fachripaul@gmail.com>
2025-03-27 13:53:52 +0700

    Initial Commit

---
+++ ImageEditingIOS/ImageEditingIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditingIOSApp.swift
+//  ImageEditingIOS
+//
+//  Created by Fachri Febrian on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditingIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
